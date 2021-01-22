364d9378d30546e1e741834a5725da6f5743c089
Dogukan Koc <dk.dogukoc@gmail.com>
2021-01-22 15:54:57 +0300

    first commit

---
+++ sizeof_operator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("sizeof(_Bool)						= %zu \n", sizeof(_Bool));
+	printf("sizeof(char)						= %zu \n", sizeof(char));
+	printf("sizeof(signed char)					= %zu \n", sizeof(signed char));
+	printf("sizeof(unsigned char)				= %zu \n", sizeof(unsigned char));
+	printf("sizeof(signed short int)			= %zu \n", sizeof(signed short int));
+	printf("sizeof(unsigned short int)			= %zu \n", sizeof(unsigned short int));
+	printf("sizeof(signed int)					= %zu \n", sizeof(signed int));
+	printf("sizeof(unsigned int)				= %zu \n", sizeof(unsigned int));
+	printf("sizeof(signed long int)				= %zu \n", sizeof(signed long int));
+	printf("sizeof(unsigned long int)			= %zu \n", sizeof(unsigned long int));
+	printf("sizeof(signed long long int)		= %zu \n", sizeof(signed long long int));
+	printf("sizeof(unsigned long long int)		= %zu \n", sizeof(unsigned long long int));
+	
+	return 0;
+}
